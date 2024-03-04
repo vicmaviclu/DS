9982db84d3d89b9f292d697c3d1660f0a22c49f2
Casalini <vgcasalini@correo.ugr.es>
2024-03-04 17:35:48 +0100

    eliminar bicis terminado

--- Ejercicio1/CarreraCarretera.java
+++ Ejercicio1/CarreraCarretera.java
@@ -10,8 +10,11 @@ public class CarreraCarretera extends Carrera {
 
     @Override
     public void empezarCarrera() throws InterruptedException{
-        // Dormir durante 60 segundos
-        //Thread.sleep(60000);
+        System.out.println("La carrera ha comenzado!");
+        
+        // Dormir durante 60 segundos        
+        Thread.sleep(60000);
+        
         // Eliminar el 10% de las bicicletas
         Random random = new Random();
         int nEliminadas = (int) (getBicicletas().size() * 0.10);
@@ -19,8 +22,6 @@ public class CarreraCarretera extends Carrera {
             int numeroAleatorio = random.nextInt(nEliminadas + 1 - i); // Genera un numero aleatorio entre 0 y nEliminadas -i o sea todo el espacio del array; (0, nEliminadas)
             eliminarBicicleta(numeroAleatorio);
         }
-
-        System.out.println("La carrera ha comenzado!");
 
         System.out.println("La carrera ha terminado!");
     }
--- Ejercicio1/CarreraMontaña.java
+++ Ejercicio1/CarreraMontaña.java
@@ -1,6 +1,7 @@
 package Ejercicio1;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CarreraMontaña  extends Carrera{
     public CarreraMontaña(ArrayList<Bicicleta> bicicletas) {
@@ -9,8 +10,19 @@ public class CarreraMontaña  extends Carrera{
 
     @Override
     public void empezarCarrera() throws InterruptedException{
-        // Dormir durante 60 segundos
+        System.out.println("La carrera ha comenzado!");
+        
+        // Dormir durante 60 segundos        
         Thread.sleep(60000);
-        // Eliminar el 20% de las bicis
+        
+        // Eliminar el 20% de las bicicletas
+        Random random = new Random();
+        int nEliminadas = (int) (getBicicletas().size() * 0.20);
+        for(int i = 0; i < nEliminadas; i++){
+            int numeroAleatorio = random.nextInt(nEliminadas + 1 - i); // Genera un numero aleatorio entre 0 y nEliminadas -i o sea todo el espacio del array; (0, nEliminadas)
+            eliminarBicicleta(numeroAleatorio);
+        }
+
+        System.out.println("La carrera ha terminado!");
     }
 }
