41e337d43cc18b23c8693b9910cb30fbc8ccb819
Casalini <vgcasalini@correo.ugr.es>
2024-03-06 14:00:57 +0100

    ejercicio5 terminado

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class ScrapeStrategy():
     def scrape(self, url):
@@ -28,12 +29,32 @@ class BeautifulSoupStrategy(ScrapeStrategy):
         else:
             return f'Failed to retrieve the webpage, status code: {response.status_code}'
 
-
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class SeleniumStrategy(ScrapeStrategy):
     def scrape(self, url):
-        pass
+        driver = webdriver.Chrome()
+        driver.get(url)
+        driver.implicitly_wait(30)
 
+        boton_cookies_aceptar = driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/div[2]/div[2]/button[1]") # /html/body/div/div/div/div/form/div[2]/div[2]/button[1]
+        boton_cookies_aceptar.click()     
+   
+        # div = driver.find_element(By.XPATH, "//div[@id='quote-summary']")        
+
+        open_value_td = driver.find_element(By.XPATH, ".//td[@data-test='OPEN-value']")
+        volume = driver.find_element(By.XPATH, ".//td[@data-test='TD_VOLUME-value']")
+        market_cap = driver.find_element(By.XPATH, ".//td[@data-test='MARKET_CAP-value']")
+        previous_close = driver.find_element(By.XPATH, ".//td[@data-test='PREV_CLOSE-value']")
+        if (open_value_td, previous_close, volume, market_cap):
+            return {
+                'OPEN-value': open_value_td.text.strip(),
+                'PREV_CLOSE-value': previous_close.text.strip(),
+                'Volume': volume.text.strip(),
+                'Market_cap': market_cap.text.strip()
+            }
+        else:
+            return 'Values not found'
 
 class Context:
     def __init__(self, strategy):
@@ -46,47 +67,22 @@ class Context:
         return self._strategy.scrape(url)
 
 
+# Estrategia scraping con BeautifulSoup
+    
 url = 'https://finance.yahoo.com/quote/TSLA'
 context = Context(BeautifulSoupStrategy())
 results = context.scrape(url)
 
+print("Resultados con BeautifulSoup: ")
 for key,value in results.items():
     print(f'{key}: {value}')
 
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+# Estrategia scraping con Selenium
 
-
-driver = webdriver.Chrome()
-driver.get('https://finance.yahoo.com/quote/TSLA')
-
-driver.implicitly_wait(10)
-boton_cookies_aceptar = driver.find_element(By.XPATH, "//button[@class='btn secondary accept-all ']") # /html/body/div/div/div/div/form/div[2]/div[2]/button[1]
-boton_cookies_aceptar.click()
-driver.implicitly_wait(10)
-div = driver.find_element(By.XPATH, "//div[@id='quote-summary']")
-tablas = div.find_element(By.XPATH, "//tr[@class='Bxz(bb) Bdbw(1px) Bdbs(s) Bdc($seperatorColor) H(36px) ']")
-
-open_value_td = div.find_element(By.XPATH, ".//td[@data-test='OPEN-value']")
-volume = div.find_element(By.XPATH, ".//td[@data-test='TD_VOLUME-value']")
-market_cap = div.find_element(By.XPATH, ".//td[@data-test='MARKET_CAP-value']")
-previous_close = div.find_element(By.XPATH, ".//td[@data-test='PREV_CLOSE-value']")
-print(open_value_td.text)
-print(previous_close.text)
-print(volume.text)
-print(market_cap.text)
-
-
-
-driver.close()
-
-# Resto del código sigue igual
-"""
-url = 'https://finance.yahoo.com/quote/TSLA'
 context = Context(SeleniumStrategy())
 results = context.scrape(url)
 
+print("Resultados con Selenium: ")
 for key, value in results.items():
     print(f'{key}: {value}')
-"""
