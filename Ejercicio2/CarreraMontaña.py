039d9eea24dac764e44c982b12e4091c569b767f
kaitolorenzo03 <kaitolorenzo03@correo.ugr.es>
2024-03-11 17:11:27 +0100

    clasespy

--- Ejercicio2/CarreraMontaña.py
+++ Ejercicio2/CarreraMontaña.py
@@ -1,5 +1,20 @@
 import Carrera
+import random
 
 class CarreraMontaña(Carrera):
-    def empezarCarrera():pass
+    def empezarCarrera(self):
+        print(f"La carrera de Montaña ha comenzado con el siguiente número de bicicletas: {len(self.bicicletas)}")
+
+        # Sleep for 60 seconds
+        # time.sleep(60)
+
+        # Remove 10% of the bicycles
+        nEliminadas = int(len(self.bicicletas) * 0.10)
+        for _ in range(nEliminadas):
+            numeroAleatorio = random.randint(0, len(self.bicicletas) - 1)  # Generates a random number between 0 and size; (0, size]
+            self.eliminarBicicleta(numeroAleatorio)
+
+        print(f"La carrera de Montaña ha terminado con el siguiente número de bicicletas: {len(self.bicicletas)}")
+        
+        
 
