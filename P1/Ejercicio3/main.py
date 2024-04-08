c7d6505147a67e6b12aca5fd8911812017e1460e
Casalini <vgcasalini@correo.ugr.es>
2024-04-08 13:18:33 +0200

    3

--- P1/Ejercicio3/main.py
+++ P1/Ejercicio3/main.py
@@ -5,7 +5,6 @@ from cuatro_quesos_builder import CuatroQuesosBuilder
 
 # Crea un builder para una pizza Margarita
 margarita_builder = MargaritaBuilder()
-# Configura el director para usar el builder de la pizza Margarita
 director = Director(margarita_builder)
 # Construye la pizza y la imprime
 pizza = director.build_pizza()
@@ -13,7 +12,6 @@ print(pizza)
 
 # Crea un builder para una pizza Cuatro Quesos
 cuatro_quesos_builder = CuatroQuesosBuilder()
-# Configura el director para usar el builder de la pizza Cuatro Quesos
 director = Director(cuatro_quesos_builder)
 # Construye la pizza y la imprime
 pizza = director.build_pizza()
