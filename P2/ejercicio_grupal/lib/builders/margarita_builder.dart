0057d7dc3495f2781ba98e2bffa5491d0def3cae
Joaquin <joaquinsalas@correo.ugr.es>
2024-04-15 17:24:01 +0200

    builders

---
+++ P2/ejercicio_grupal/lib/builders/margarita_builder.dart
@@ -0,0 +1,16 @@
+import 'package:ejercicio_grupal/models/pizza.dart';
+import 'pizza_builder.dart';
+
+class MargaritaBuilder extends PizzaBuilder {
+  Pizza? pizza;
+
+  @override
+  void createNewPizza() {
+    pizza = Pizza();
+  }
+
+  @override
+  void addIngredientes(List<String> Ingredientes) {
+    pizza?.ingredientes = Ingredientes;
+  }
+}
