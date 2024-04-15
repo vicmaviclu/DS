898fef800ee0f84fc81d35f8b629f5f74006849a
Casalini <vgcasalini@correo.ugr.es>
2024-04-15 16:50:49 +0200

    menu

--- P2/ejercicio_grupal/lib/menu.dart
+++ P2/ejercicio_grupal/lib/menu.dart
@@ -1,3 +1,22 @@
+import 'models/pizza.dart';
 
+class Menu {
+  List<Pizza> pizzas;
 
+  Menu(this.pizzas);
 
+  void addPizza(Pizza pizza) {
+    pizzas.add(pizza);
+  }
+
+  void removePizza(Pizza pizza) {
+    pizzas.remove(pizza);
+  }
+
+  void updatePizza(Pizza oldPizza, Pizza newPizza) {
+    int index = pizzas.indexOf(oldPizza);
+    if (index != -1) {
+      pizzas[index] = newPizza;
+    }
+  }
+}
