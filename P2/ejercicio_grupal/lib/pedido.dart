b8aef289db78dd43af0005bd7a0da43118b1877b
Casalini <vgcasalini@correo.ugr.es>
2024-04-20 12:30:11 +0200

    12:30 20/04

--- P2/ejercicio_grupal/lib/pedido.dart
+++ P2/ejercicio_grupal/lib/pedido.dart
@@ -2,10 +2,13 @@ import 'sistema_envios.dart';
 import 'sistema_pagos.dart';
 
 class Pedido {
+  static int _contadorPedidos = 0; // Variable de clase para llevar la cuenta de los pedidos
+  final int numeroPedido; // Variable de instancia para el número de este pedido
   final String pizzaSeleccionada;
   final String tamanoSeleccionado;
   final SistemaEnvios sistemaEnvios;
   final SistemaPagos sistemaPagos;
+  final String numeroTelefono; // Nueva variable de instancia
 
   Pedido({
     required this.pizzaSeleccionada,
@@ -13,25 +16,25 @@ class Pedido {
     required String direccion,
     required String tarjeta,
     required double coste,
-  })  : sistemaEnvios = SistemaEnvios(direccion: direccion),
-        sistemaPagos = SistemaPagos(tarjeta: tarjeta, coste: coste);
+    required this.numeroTelefono, // Actualiza el constructor
+  })  : sistemaEnvios = SistemaEnvios(direccion: direccion, numeroTelefono: numeroTelefono), // Actualiza la creación de la instancia de SistemaEnvios
+        sistemaPagos = SistemaPagos(tarjeta: tarjeta, coste: coste),
+        numeroPedido = ++_contadorPedidos; // Incrementa el contador y asigna el valor a numeroPedido
 
   void hacerPedido() {
     sistemaPagos.procesarPago();
     sistemaEnvios.enviarPedido();
-    // LLamar al builder correspondiente para hacer la pizza y devolverla
-    // Se puede hacer un controlador de builders que llame al builder correspondiente
-    // o hacer unos builders que te hagan la pizza basica con cada tamaño
-    // y luego hacer el patron decorador para los ingedientes(decoradorPepperoni, decoradorMargarita, etc)
     print('pedido realizado');
   }
 
   @override
   String toString() {
     return '\n'
+           'Número de pedido: $numeroPedido\n' // Añade el número de pedido a la salida
            'Pizza seleccionada: $pizzaSeleccionada\n'
            'Tamaño seleccionado: $tamanoSeleccionado\n'
            'Dirección: ${sistemaEnvios.direccion}\n'
+           'Número de teléfono: $numeroTelefono\n' // Añade el número de teléfono a la salida
            'Tarjeta: ${sistemaPagos.tarjeta}\n'
            'Coste: ${sistemaPagos.coste}';
   }
--- P2/ejercicio_grupal/lib/sistema_envios.dart
+++ P2/ejercicio_grupal/lib/sistema_envios.dart
@@ -1,9 +1,11 @@
 class SistemaEnvios {
   final String direccion;
+  final String numeroTelefono; 
 
-  SistemaEnvios({required this.direccion});
+  SistemaEnvios({required this.direccion, required this.numeroTelefono}); 
 
   void enviarPedido() {
     print('Enviando el pedido a la dirección $direccion');
+    print('Número de teléfono de contacto: $numeroTelefono');
   }
 }
