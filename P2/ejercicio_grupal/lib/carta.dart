8de213934c5d0540e614086cc9f2faab45921bb9
Casalini <vgcasalini@correo.ugr.es>
2024-04-20 10:38:40 +0200

    singleton carta

--- P2/ejercicio_grupal/lib/carta.dart
+++ P2/ejercicio_grupal/lib/carta.dart
@@ -7,42 +7,22 @@ class PizzaConFoto {
   PizzaConFoto({required this.pizza, required this.foto});
 }
 
+// Carta clase singleton 
 class Carta {
   List<PizzaConFoto> pizzas;
 
-  Carta() : pizzas = [
+  static final Carta _singleton = Carta._internal();
+
+  factory Carta() {
+    return _singleton;
+  }
+
+  Carta._internal() : pizzas = [
     PizzaConFoto(
       pizza: Pizza(
         nombre: 'Margarita',
         precio: 8.50,
         ingredientes: ['Tomate', 'Mozzarella', 'Albahaca'],
-        tamano: '',
-      ),
-      foto: 'assets/descarga.png',
-    ),
-    PizzaConFoto(
-      pizza: Pizza(
-        nombre: 'Pepperoni',
-        precio: 9.00,
-        ingredientes: ['Tomate', 'Mozzarella', 'Pepperoni'],
-        tamano: '',
-      ),
-      foto: 'assets/descarga.png',
-    ),
-    PizzaConFoto(
-      pizza: Pizza(
-        nombre: 'Pepperoni',
-        precio: 9.00,
-        ingredientes: ['Tomate', 'Mozzarella', 'Pepperoni'],
-        tamano: '',
-      ),
-      foto: 'assets/descarga.png',
-    ),
-    PizzaConFoto(
-      pizza: Pizza(
-        nombre: 'Pepperoni',
-        precio: 9.00,
-        ingredientes: ['Tomate', 'Mozzarella', 'Pepperoni'],
         tamano: '',
       ),
       foto: 'assets/descarga.png',
